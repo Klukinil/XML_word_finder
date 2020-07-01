8c41a777a652b989adbdbd7269dacb977ab66216
klukinil <klukinil@gmail.com>
2020-07-01 09:48:01 +0000

    Function added

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import xml.etree.ElementTree as ET
+def word_finder(xml_file,word_length, top):
+  parser = ET.XMLParser(encoding="utf-8")
+  tree = ET.parse(xml_file, parser)
+  root = tree.getroot()
+  items_list2 = root.findall("channel/item/description")
+  text = []
+  for description in items_list2:
+    text.append(description.text)
+  words_list = []
+  for news in text:
+    words_list.append(news.split())
+  words = words_list[0]
+  for i in range(1, len(words_list)):
+    words += words_list[i]
+    i +=1
+  long_words = []
+  for word in words:
+    if len(word) >= 6:
+      long_words.append(word)
+  long_words.sort()
+  words_count = dict()
+  for  long_word in long_words:
+    words_count[long_word] = long_words.count(long_word)
+  value_count = []
+  for value in words_count.values():
+    value_count.append(value)
+  value_count.sort(reverse=True)
+  top_value = value_count[0:10]
+  top_word = dict()
+  for key,value in words_count.items():
+    if value in top_value:
+      top_word[key] = value
+  print(top_word)
 
-parser = ET.XMLParser(encoding="utf-8")
-tree = ET.parse("newsafr.xml", parser)
-root = tree.getroot()
-
-items_list2 = root.findall("channel/item/description")
-print(len(items_list2))
-print(items_list2)
-
-for description in items_list2:
-  print(description.text)
+word_finder("newsafr.xml", 6, 10)
